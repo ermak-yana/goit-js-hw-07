4bfaef542ddb15249f2332a9f5a7c41d96ebf360
ermak-yana <lidl.peperts.stor@gmail.com>
2021-07-25 18:09:05 +0300

    3 задачи

---
+++ js/task-02.js
@@ -0,0 +1,22 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',];
+    const listIngredientsRef = document.querySelector('#ingredients');
+
+    const elements = ingredients.map(element => {
+      const itemEl = document.createElement('li');
+      itemEl.textContent = element;
+       
+      return itemEl;
+    });
+     
+    listIngredientsRef.append(...elements);
+    
+    console.log(listIngredientsRef);
+
+    
+  
